e8eb62105954dccb948dfb85a0c908e3c7d3dc8f
chechogarcia <65240206+chechogarcia@users.noreply.github.com>
2022-05-05 16:08:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from functions import Damas
 
-
-
-
-d = Damas()
-
+c = Damas()
+I ={c.DenC.P([0,1]): True, c.DenC.P([1,3]): True, c.DenC.P([2,5]): True, c.DenC.P([3,7]): True, c.DenC.P([4,2]): True, c.DenC.P([5,0]): True, c.DenC.P([6,6]): True, c.DenC.P([7,4]): True}
+c.visualizar(I)
